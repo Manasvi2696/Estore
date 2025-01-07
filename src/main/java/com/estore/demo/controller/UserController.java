59c56aa9b97064ac91bbe558ee992061035ea72a
Manasvi khot <manasvi254@gmail.com>
2025-01-07 18:25:53 +0530

    Added logger

--- src/main/java/com/estore/demo/controller/UserController.java
+++ src/main/java/com/estore/demo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.estore.demo.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,11 +22,14 @@ import com.estore.demo.service.UserService;
 @RequestMapping("/User")
 public class UserController {
 
+	private static final Logger logger = LogManager.getLogger(UserController.class);
+
 	@Autowired
 	private UserService userservice;
 	
 	@GetMapping("/all")
 	public List<User> getUser(){
+		logger.info("Fatching All Users...");
 		return userservice.getAll();
 	}
 	
@@ -32,25 +37,31 @@ public class UserController {
 	public ResponseEntity<User> getUserById(@PathVariable int id){
 		Optional<User> user = userservice.getbyId(id);
 		if (user.isPresent()) {
+			logger.info("Fatching User with id : "+id);
             return ResponseEntity.ok(user.get());
         } else {
+			logger.info("User with id : "+id+" is not found");
             return ResponseEntity.notFound().build();
         }
 	}
 	
 	@PostMapping("/CreateUser")
 	public User CreateUser(@RequestBody User user) {
+		logger.info("Creating new User...");
 		return userservice.Create(user);
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deleteProduct(@PathVariable int id) {
+		logger.info("Deleting user with id:"+id);
 	Optional<User> user = userservice.getbyId(id);
     if (user.isPresent()) {
     	userservice.deleteById(id);
+		logger.info("User with id : "+id+" deleted");
         return ResponseEntity.noContent().build();
     } else {
+		logger.info("User with id : "+id+" not found");
         return ResponseEntity.notFound().build();
     }
-	}
+    }
 }
